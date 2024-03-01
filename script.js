ae9e32eef8b97cbb49043aa5b1cd0ed6f601a2a2
Emmanuel Anaba <emmaanaba29@gmail.com>
2024-03-01 10:56:26 +0100

    completely done

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-p = document.querySelectorAll("p");
-blockquote = document.querySelectorAll("blockquote");
-
-// Define the URL of the API you want to call
+const p = document.querySelectorAll("p");
+const blockquote = document.querySelectorAll("blockquote");
 const apiUrl = "https://type.fit/api/quotes";
 
-// Make a GET request to the API
 fetch(apiUrl)
   .then((response) => {
-    // Check if the request was successful
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
-    // Parse the JSON response
     return response.json();
   })
   .then((data) => {
-    // Handle the data returned by the API
-    console.log(data);
     for (i = 0; i < p.length; i++) {
       p[i].textContent = data[i % 5].text;
       blockquote[i].textContent = data[i % 5].author.substring(
@@ -26,6 +19,5 @@ fetch(apiUrl)
     }
   })
   .catch((error) => {
-    // Handle any errors that occurred during the fetch
     console.error("There was a problem with the fetch operation:", error);
   });
